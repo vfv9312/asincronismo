00ad8cbd97a5b0d408bcacaea792b4ae2010ab6a
Vladimir Farrera <vfv9312@gmail.com>
2023-05-26 10:21:39 -0600

    segundo commit

---
+++ src/Generators/index.js
@@ -0,0 +1,31 @@
+//Declaración de la función del Generador
+function* gen(){
+	yield 1;
+	yield 2;
+	yield 3;
+}
+//Expresión de la función Generadora
+const g = gen();
+//Llamada del método next en el objeto del Generador
+console.log(g.next()); //Imprime el primer yield: {value: 1, done: false} 
+//Llamada del método next en el objeto del Generador
+console.log(g.next().value); //Imprime el primer yield: 1
+console.log(g.next().value); //Imprime el segundo yield: 2
+console.log(g.next().value); //Si se coloca un cuarto console, la consola indica "Undefined"
+
+//---------------------------------------------------------------
+//Declaración de la función del Generador pasando un argumento
+function* iterate(array){
+    for(let value of array){ //El loop del for revisa cada elemento del array
+        yield value; //value es asignado en cada ciclo
+    }
+}
+
+const it = iterate(['Oscar', 'Omar', 'Ana', 'Lucia', 'Juan']); 
+
+//la diferencia con el ejemplo anterior es que el iterador se le pasa un argumento
+console.log(it.next().value); //Imprime el primer elemento del array: Oscar
+console.log(it.next().value); //Imprime el segundo elemento del array: Omar
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value); //Si se coloca un sexto console, la consola indica "Undefined"
